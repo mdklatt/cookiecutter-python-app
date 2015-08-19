f063dc1dd75455eac8cb85f24d329e369cfc8f26
Michael Klatt <mdklatt@ou.edu>
2015-08-19 03:45:54 -0500

    added template test suite

---
+++ {{cookiecutter.repo_name}}/test/test_{{cookiecutter.repo_name}}.py
@@ -0,0 +1,55 @@
+""" Test suite for the {{cookiecutter.repo_name}} application template.
+
+The script can be executed on its own or incorporated into a larger test suite.
+However the tests are run, be aware of which version of the package is actually
+being tested. If the package is installed in site-packages, that version takes
+precedence over the version in this project directory. Use a virtualenv test
+environment or setuptools develop mode to test against the development version.
+
+"""
+import pytest
+
+
+def test_version():
+    """ Test the application version.
+
+    """
+    from {{cookiecutter.repo_name}} import __version__
+    assert __version__ == "{{cookiecutter.project_version}}"
+    return
+
+
+def test_main():
+    """ Test the main entry point.
+
+    """
+    from {{cookiecutter.repo_name}} import main
+    with pytest.raises(NotImplementedError):
+        main()
+    return
+
+
+def test_cli():
+    """ Test the CLI entry point.
+
+    """
+    from {{cookiecutter.repo_name}}.cli import main
+    with pytest.raises(NotImplementedError):
+        main()
+    return
+
+
+def test_gui():
+    """ Test the GUI entry point.
+
+    """
+    from {{cookiecutter.repo_name}}.gui import main
+    with pytest.raises(NotImplementedError):
+        main()
+    return
+
+
+# Make the module executable.
+
+if __name__ == "__main__":
+    raise SystemExit(pytest.main(__file__))
