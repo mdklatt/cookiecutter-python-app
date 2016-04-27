848d54b42d8b9d1437d50b6fcdb85403efe1c486
Michael Klatt <mdklatt@ou.edu>
2016-04-27 14:56:26 -0500

    modified setup.py in template project to use 'cli_script' parameter

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -28,7 +28,7 @@ _CONFIG = {
     "package_dir": {"": "lib"},
     "packages": find_packages("lib"),
     "entry_points": {
-        "console_scripts": ("cli = {{ cookiecutter.app_name }}.cli:main",),
+        "console_scripts": ("{{ cookiecutter.cli_script }} = {{ cookiecutter.app_name }}.cli:main",),
         "gui_scripts": ("gui = {{ cookiecutter.app_name }}.gui:main",)
     },
     "data_files": [(root, map(partial(join, root), paths)) for (root, paths)
