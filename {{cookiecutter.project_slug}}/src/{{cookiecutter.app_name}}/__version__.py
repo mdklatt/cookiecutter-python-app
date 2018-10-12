94db27547233855d4bd1bea0ea4967d446685759
Michael Klatt <mdklatt@alumni.ou.edu>
2018-10-11 20:01:14 -0500

    Tweaked __version__.py in template application

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.app_name}}/__version__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.app_name}}/__version__.py
@@ -8,7 +8,8 @@ This project uses the Semantic Versioning scheme in conjunction with PEP 0440:
 Major versions introduce significant changes to the API, and backwards 
 compatibility is not guaranteed. Minor versions are for new features and other
 backwards-compatible changes to the API. Patch versions are for bug fixes and
-internal code changes that do not affect the API.
+internal code changes that do not affect the API. Development versions are
+incomplete states of a release .
 
 Version 0.x should be considered a development version with an unstable API,
 and backwards compatibility is not guaranteed for minor versions.
