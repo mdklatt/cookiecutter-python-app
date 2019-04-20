a667c6a4ebbc40295e43ac6a0a18cecd8088e82f
Michael Klatt <mdklatt@alumni.ou.edu>
2019-04-20 16:23:47 -0500

    Refactored setup.py in project

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,30 +1,14 @@
 """ Setup script for the {{ cookiecutter.app_name }} application.
 
 """
-from distutils import log
 from itertools import chain
-from os import walk
-from os.path import join
-from subprocess import check_call
-from subprocess import CalledProcessError
+from pathlib import Path
 
-from setuptools import Command
 from setuptools import find_packages
 from setuptools import setup
 
 
-def _listdir(root):
-    """ Recursively list all files under 'root'.
-
-    """
-    for path, _, names in walk(root):
-        yield path, tuple(join(path, name) for name in names)
-    return
-
-
-_DATA = "etc/",
-
-_CONFIG = {
+_config = {
     "name": "{{ cookiecutter.app_name }}",
     "author": "{{ cookiecutter.author_name }}",
     "author_email": "{{ cookiecutter.author_email }}",
@@ -34,94 +18,31 @@ _CONFIG = {
     "entry_points": {
         "console_scripts": ("{{ cookiecutter.cli_script }} = {{ cookiecutter.app_name }}.cli:main",),
     },
-    "data_files": list(chain.from_iterable(_listdir(root) for root in _DATA))
+    "data_files": ("etc/",),
 }
 
 
-def _version():
-    """ Get the local package version.
-
-    """
-    path = join("src", _CONFIG["name"], "__version__.py")
-    namespace = {}
-    with open(path) as stream:
-        exec(stream.read(), namespace)
-    return namespace["__version__"]
-
-
-class _CustomCommand(Command):
-    """ Abstract base class for a custom setup command.
-
-    """
-    # Each user option is a tuple consisting of the option's long name (ending
-    # with "=" if it accepts an argument), its single-character alias, and a
-    # description.
-    description = ""
-    user_options = []  # this must be a list
-
-    def initialize_options(self):
-        """ Set the default values for all user options.
-
-        """
-        return
-
-    def finalize_options(self):
-        """ Set final values for all user options.
-
-        This is run after all other option assignments have been completed
-        (e.g. command-line options, other commands, etc.)
-
-        """
-        return
-
-    def run(self):
-        """ Execute the command.
-
-        Raise SystemExit to indicate failure.
-
-        """
-        raise NotImplementedError
-
-
-class UpdateCommand(_CustomCommand):
-    """ Custom setup command to pull from a remote branch.
-
-    """
-    description = "update from a remote branch"
-    user_options = [
-        ("remote=", "r", "remote name [default: tracking remote]"),
-        ("branch=", "b", "branch name [default: tracking branch]"),
-    ]
-
-    def initialize_options(self):
-        """ Set the default values for all user options.
-
-        """
-        self.remote = ""  # default to tracking remote
-        self.branch = ""  # default to tracking branch
-        return
-
-    def run(self):
-        """ Execute the command.
-
-        """
-        args = {"remote": self.remote, "branch": self.branch}
-        cmdl = "git pull --ff-only {remote:s} {branch:s}".format(**args)
-        try:
-            check_call(cmdl.split())
-        except CalledProcessError:
-            raise SystemExit(1)
-        log.info("package version is now {:s}".format(_version()))
-        return
-
-
 def main():
-    """ Execute the setup commands.
+    """ Execute the setup command.
 
     """
-    _CONFIG["version"] = _version()
-    _CONFIG["cmdclass"] = {"update": UpdateCommand}
-    setup(**_CONFIG)
+    def files(*dirs):
+        """ Recursively list all files. """
+        dirs = (Path(root).rglob("*") for root in dirs)
+        return map(str, chain.from_iterable(dirs))
+
+    def version():
+        """ Get the local package version. """
+        namespace = {}
+        path = Path("src", _config["name"], "__version__.py")
+        exec(path.read_text(), namespace)
+        return namespace["__version__"]
+
+    _config.update({
+        "data_files": list(files(*_config["data_files"])),  # expand files
+        "version": version(),
+    })
+    setup(**_config)
     return 0
 
 
