e3420ede4dfaf8280541be28d36e3524597147a3
Michael Klatt <mdklatt@alumni.ou.edu>
2019-06-04 10:00:05 -0500

    Added `project_url` template parameter

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -10,9 +10,9 @@ from setuptools import setup
 
 _config = {
     "name": "{{ cookiecutter.app_name }}",
+    "url": "{{ cookiecutter.project_url }}",
     "author": "{{ cookiecutter.author_name }}",
     "author_email": "{{ cookiecutter.author_email }}",
-    "url": "",
     "package_dir": {"": "src"},
     "packages": find_packages("src"),
     "entry_points": {
@@ -22,7 +22,7 @@ _config = {
 }
 
 
-def main():
+def main() -> int:
     """ Execute the setup command.
 
     """
