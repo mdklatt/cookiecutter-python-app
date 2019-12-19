acf9b9f2d7a15cfd1b3a535c713076f091ca7e2c
Michael Klatt <mdklatt@alumni.ou.edu>
2019-12-18 19:39:45 -0600

    Fixed core.logger test to avoid modifying global object

--- {{cookiecutter.project_slug}}/test/{{cookiecutter.app_name}}/core/test_logger.py
+++ {{cookiecutter.project_slug}}/test/{{cookiecutter.app_name}}/core/test_logger.py
@@ -14,16 +14,24 @@ import pytest
 from {{ cookiecutter.app_name }}.core.logger import *  # tests __all__
 
 
+@pytest.fixture
+def logger():
+    """ Return a Logger object for testing.
+
+    """
+    # Don't modify global object.
+    return Logger()
+
+
 class LoggerTest(object):
     """ Test suite for the Logger class.
     
     """
-    def test_start(self, capsys):
+    def test_start(self, capsys, logger):
         """ Test the start method.
         
         """
         message = "test message"
-        logger = Logger()
         logger.start("debug")
         logger.debug(message)
         _, stderr = capsys.readouterr()
@@ -31,11 +39,10 @@ class LoggerTest(object):
         assert message in stderr
         return
 
-    def test_stop(self, capsys):
+    def test_stop(self, capsys, logger):
         """ Test the stop() method.
         
         """
-        logger = Logger()
         logger.start("debug")
         logger.stop()
         logger.critical("test")
@@ -43,7 +50,7 @@ class LoggerTest(object):
         assert not stderr
         return
 
-    def test_restart(self, capsys):
+    def test_restart(self, capsys, logger):
         """ Test a restart.
 
         """
@@ -56,7 +63,7 @@ class LoggerTest(object):
         assert message not in stderr
         return
 
-    def test_stream(self):
+    def test_stream(self, logger):
         """ Test output to an alternate stream.
         
         """
