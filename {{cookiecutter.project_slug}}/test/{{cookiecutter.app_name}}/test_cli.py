54a67fb908f4e2d4985e8ac467c6d1fc47a64a51
Michael Klatt <mdklatt@alumni.ou.edu>
2020-04-29 20:05:12 -0500

    Updated tests for cli.py module

--- {{cookiecutter.project_slug}}/test/{{cookiecutter.app_name}}/test_cli.py
+++ {{cookiecutter.project_slug}}/test/{{cookiecutter.app_name}}/test_cli.py
@@ -7,6 +7,7 @@ precedence over the version in this project directory. Use a virtualenv test
 environment or setuptools develop mode to test against the development version.
 
 """
+from shlex import split
 from subprocess import call
 from sys import executable
 
@@ -14,7 +15,7 @@ import pytest
 from {{ cookiecutter.app_name }}.cli import *  # test __all__
 
 
-@pytest.fixture(params=("hello",))
+@pytest.fixture(params=("--help", "hello"))
 def command(request):
     """ Return the command to run.
 
@@ -26,11 +27,20 @@ def test_main(command):
     """ Test the main() function.
 
     """
-    # Call with the --help option as a basic sanity check.
-    with pytest.raises(SystemExit) as exinfo:
-        main(("{:s}".format(command), "--help"))
-    assert 0 == exinfo.value.code
+    try:
+        status = main(split(command))
+    except SystemExit as ex:
+        status = ex.code
+    assert status == 0
     return
+
+def test_main_none():
+    """ Test the main() function with no arguments.
+    
+    """
+    with pytest.raises(SystemExit) as exinfo:
+        main([])  # displays a help message and exits gracefully
+    assert exinfo.value.code == 1
 
 
 def test_script(command):
