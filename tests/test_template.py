420eb1089123dd1946e91f8bbceb5ed161f1225f
Michael Klatt <mdklatt@alumni.ou.edu>
2023-06-20 00:57:38 -0500

    Modified template tests to use pytest

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,41 +5,102 @@ installed into a self-contained venv environment, and the application test
 suite is run.
 
 """
-from pathlib import Path
-from shlex import split
-from subprocess import check_call
-from tempfile import TemporaryDirectory
-from venv import create
-
 from cookiecutter.generate import generate_context
 from cookiecutter.main import cookiecutter
+from pathlib import Path
+from shlex import split
+from subprocess import run
+from venv import create
+
+import pytest
 
 
-def main() -> int:
-    """ Execute the test.
-    
+@pytest.fixture(scope="session")
+def template() -> Path:
+    """ The template under test.
+
     """
-    template = Path(__file__).resolve().parents[1]
+    return Path(__file__).resolve().parents[1]
+
+
+@pytest.fixture(scope="module")
+def tmpdir(tmp_path_factory) -> Path:
+    """ Test directory.
+
+    """
+    return tmp_path_factory.mktemp("test_template")
+
+
+@pytest.fixture(scope="module")
+def context(template) -> dict:
+    """ Template context for testing.
+
+    """
     context = generate_context(template.joinpath("cookiecutter.json"))
-    context = context["cookiecutter"]
-    context.update({
+    context["cookiecutter"].update({
+        "app_name": context["cookiecutter"]["project_slug"],
         "author_name": "J. Smith",
         "author_email": "jsmith@example.com",
         "project_url": "http://project.example.com",
     })
-    with TemporaryDirectory() as tmpdir:
-        cookiecutter(str(template), no_input=True, output_dir=tmpdir, extra_context=context)
-        cwd = Path(tmpdir) / context["project_slug"]
-        venv = cwd / ".venv"
-        create(venv, with_pip=True)
-        python = venv / "bin" / "python"
-        install = f"{python} -m pip install -e \".[dev]\""
-        check_call(split(install), cwd=cwd)
-        check_call(split(f"{python} -m pytest -v tests"), cwd=cwd)
-    return 0
-    
-    
+    return context["cookiecutter"]
+
+
+@pytest.fixture(scope="module")
+def project(tmpdir, template, context) -> Path:
+    """ Create a test project from the Cookiecutter template.
+
+    """
+    cookiecutter(str(template), no_input=True, output_dir=tmpdir, extra_context=context)
+    return tmpdir / context["project_slug"]
+
+
+@pytest.fixture
+def python(tmp_path):
+    """ Create a Python virtual environment for testing.
+
+    """
+    venv = tmp_path / ".venv"
+    create(venv, with_pip=True)
+    return venv / "bin" / "python"
+
+
+def test_project(project):
+    """ Verify that the project was created correctly.
+
+    """
+    # Just a basic sanity test.
+    assert len(list(project.iterdir())) == 8
+    return
+
+
+def test_dev(project, python):
+    """ Test the creation of the project dev environment.
+
+    """
+    install = f"pip install -e .[dev]"
+    test = "pytest tests/"
+    for command in install, test:
+        args = split(f"{python} -m {command}")
+        process = run(args, cwd=project)
+        assert process.returncode == 0
+    return
+
+
+def test_app(context, project, python):
+    """ Test the installed application.
+
+    """
+    install = f"pip install {project}"
+    cli = f"{context['app_name']}.cli --help"
+    for command in install, cli:
+        args = split(f"{python} -m {command}")
+        process = run(args)
+        assert process.returncode == 0
+    return
+
+
 # Make the script executable.
 
 if __name__ == "__main__":
-    raise SystemExit(main())
+    raise SystemExit(pytest.main([__file__]))
